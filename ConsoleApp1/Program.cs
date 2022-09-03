038d82e4c4591b5eeba3bc162d3e0e420f14bf13
BeskonchinRV <BeskonchinRV@313D-11>
2022-09-03 12:17:39 +0700

    Завершение урока

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,21 @@
 ﻿using System;
 
-Console.WriteLine("Hello World");
+int x, y;
+Console.WriteLine("Введите x");
+x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите y");
+y = Convert.ToInt32(Console.ReadLine());
 
+//Вариант 1
+//int z = x;
+//x = y;
+//y = x;
+
+//Вариант 2
+//x = x - y;
+//y = x + y;
+//x = y - x
+
+Console.WriteLine($"x = {x}, y = {y}");
+
+Console.ReadKey();
